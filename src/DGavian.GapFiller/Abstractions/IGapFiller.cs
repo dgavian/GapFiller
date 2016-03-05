1e98bd27de8cfa586a8f2aa031b57b7f5c2295c8
David Goldstein <dgavian@hotmail.com>
2016-03-05 11:46:22 -0700

    GapFiller project initial commit

---
+++ src/DGavian.GapFiller/Abstractions/IGapFiller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DGavian.GapFiller.Abstractions
+{
+    public interface IGapFiller<T>
+     where T : IOffset, new()
+    {
+        void FillGaps(List<T> items);
+        void FillGaps(List<T> items, Func<double, T> getDefaultRecord);
+    }
+}
---
+++ src/DGavian.GapFiller/Abstractions/IOffset.cs
@@ -0,0 +1,7 @@
+﻿namespace DGavian.GapFiller.Abstractions
+{
+    public interface IOffset
+    {
+        double Offset { get; set; }
+    }
+}
